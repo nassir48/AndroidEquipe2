ada122591ea4c186d18976742e3469bee7a85a80
nassir48 <mamadounassir.diallo@uadb.edu.sn>
2020-02-02 19:07:20 +0100

    ter

--- GoByTER/app/src/main/java/com/go/by/ter/OptionReservationActivity.java
+++ GoByTER/app/src/main/java/com/go/by/ter/OptionReservationActivity.java
@@ -2,13 +2,24 @@ package com.go.by.ter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OptionReservationActivity extends AppCompatActivity {
-
+Button btn_generer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option_reservation);
+        btn_generer= findViewById(R.id.generer_recu);
+        btn_generer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(OptionReservationActivity.this,RecuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
