e423aeba72ab8ccd612b3e137addb70b588a26c4
Rjal95 <fallmodou950@gmail.com>
2020-02-02 19:44:10 +0000

    les arrets modifs

--- GoByTER/app/src/main/java/com/go/by/ter/MainActivity.java
+++ GoByTER/app/src/main/java/com/go/by/ter/MainActivity.java
@@ -53,6 +53,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, LocalisationArretActivity.class);
+                startActivity(intent);
+            }
+        });
 
         String UserNameKey = Paper.book().read(Prevalent.UserNameKey);
         String UserPassword = Paper.book().read(Prevalent.UserPasswordKey);
