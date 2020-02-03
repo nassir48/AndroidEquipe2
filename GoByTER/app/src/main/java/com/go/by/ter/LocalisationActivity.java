fd2cbb07b8bf00dd905255857ec8e3534606d088
nassir48 <mamadounassir.diallo@uadb.edu.sn>
2020-02-03 11:05:45 +0100

    ter

--- GoByTER/app/src/main/java/com/go/by/ter/LocalisationActivity.java
+++ GoByTER/app/src/main/java/com/go/by/ter/LocalisationActivity.java
@@ -1,5 +1,6 @@
 package com.go.by.ter;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,65 +15,85 @@ import android.widget.Toast;
 
 public class LocalisationActivity<suppressWarning> extends AppCompatActivity implements LocationListener {
     private LocationManager Lm;
+    private static final int PERMS_CALL_ID=1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_localisation );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_localisation);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    // @SuppressWarnings("MissingPermission")
     protected void onResume() {
-        super.onResume ();
-        Lm = (LocationManager) getSystemService ( LOCATION_SERVICE );
-        if (Lm.isProviderEnabled ( LocationManager.GPS_PROVIDER )) {
-            if (checkSelfPermission ( Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
-                    checkSelfPermission ( Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    Activity#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for Activity#requestPermissions for more details.
-                return;
-            }
-            Lm.requestLocationUpdates ( LocationManager.GPS_PROVIDER, 1000, 0, this );
+        super.onResume();
+        checkPermssions();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkPermssions(){
+        Lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            },PERMS_CALL_ID);
+            return;
         }
-        if (Lm.isProviderEnabled ( LocationManager.PASSIVE_PROVIDER )){
-            Lm.requestLocationUpdates ( LocationManager.PASSIVE_PROVIDER,1000, 0,this);
+        if (Lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
         }
-        if (Lm.isProviderEnabled ( LocationManager.NETWORK_PROVIDER )){
-            Lm.requestLocationUpdates ( LocationManager.NETWORK_PROVIDER,1000, 0,this);
+        if(Lm.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)){
+            Lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER,10000,0,this);
+        }
+        if(Lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            Lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,10000,0,this);
         }
     }
-    protected void onPause(){
-        super.onPause ();
-        if (Lm != null){
-            Lm.removeUpdates ( this );
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==PERMS_CALL_ID){
+           checkPermssions();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (Lm !=null){
+            Lm.removeUpdates(this);
         }
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        double laltitude =location.getAltitude ();
-        double longititude =location.getLongitude ();
-        Toast.makeText ( this,"Loacation:"+laltitude+ "/"+ longititude,Toast.LENGTH_LONG ).show ();
+        double lattitude= location.getLatitude();
+        double longitude= location.getLongitude();
+
+        Toast.makeText(this, "Localisation:"+lattitude+"/"+longitude, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
     @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
+    public void onProviderEnabled(String provider) {
 
     }
 
     @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
+    public void onProviderDisabled(String provider) {
 
     }
 }
--- GoByTER/app/src/main/java/com/go/by/ter/RecuActivity.java
+++ GoByTER/app/src/main/java/com/go/by/ter/RecuActivity.java
@@ -2,13 +2,24 @@ package com.go.by.ter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecuActivity extends AppCompatActivity {
-
+Button btn_recu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recu);
+
+        btn_recu= (Button) findViewById(R.id.btn_recu);
+        btn_recu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(RecuActivity.this,MainActivity.class);
+            }
+        });
     }
 }
